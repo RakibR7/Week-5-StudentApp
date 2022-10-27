608e5046355d93bf59d0a4ccc94b8a7c23239b3f
Rakib <G00408644@atu.ie>
2022-10-27 17:31:30 +0100

    Added a new branch and re-wrote code so it display what you type

--- src/main/java/ie/atu/StudentApp/StudentApp.java
+++ src/main/java/ie/atu/StudentApp/StudentApp.java
@@ -15,20 +15,22 @@ public class StudentApp
         Scanner inputs = new Scanner(System.in);
         String name = inputs.nextLine();
         Student StudentName = new Student();
+        System.out.println("You entered: " + name);
         StudentName.getName();
-
 
         System.out.println("Please enter your email: ");
         String email = inputs.nextLine();
         Student StudentEmail = new Student();
+        System.out.println("You entered: " + email);
         StudentEmail.getEmail();
 
         System.out.println("Please enter your course: ");
         String course = inputs.nextLine();
         Student StudentCourse = new Student();
+        System.out.println("You entered: " + course);
         StudentCourse.getCourse();
 
-        String info = name + email + course;
-        System.out.println("Information about student:  " + info);
+        //String info = name + email + course;
+        //System.out.println("Information about student:  " + info);
     }
 }
